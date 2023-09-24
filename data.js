ec9204a7789293a67d30d2901c1780368c172eac
Win10 <penumbra-29@mail.ru>
2023-09-24 17:37:15 +0300

    feat: add music and cover,play/pause JS (Sun, Sep 24, 17:37:21)

---
+++ data.js
@@ -0,0 +1,14 @@
+let songs = [
+    {
+        name: 'shapito',
+        path: 'assets/tracks/xolidayboy_shapito.mp3',
+        artist: 'xolidayboy',
+        cover: 'assets/img/Shapito.jpg'
+    },
+    {
+        name: 'Biorazlagaemy',
+        path: 'assets/tracks/Anacondaz_Biorazlagaemy.mp3',
+        artist: 'Anacondaz',
+        cover: 'assets/img/Biorazlagaemy.jpg'
+    },
+]
